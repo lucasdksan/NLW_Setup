f2663db571eb2ad13ff63e46666bca6b6fdef0b4
Lucas <lucas.silva@agencian1.com.br>
2023-01-22 01:25:25 -0300

    Stop 52min

---
+++ server/src/routes.ts
@@ -0,0 +1,58 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import dayjs from "dayjs";
+
+import { prisma } from "./libs/prisma";
+
+export async function appRoutes(app:FastifyInstance){
+    app.post("/habits", async (request)=>{
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(z.number().min(0).max(6))
+        });
+
+        const { title, weekDays } = createHabitBody.parse(request.body);
+
+        const today = dayjs().startOf("day").toDate();
+
+        await prisma.habit.create({
+            data: {
+                title,
+                cretated_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                }
+            }
+        });
+    });
+
+    app.get("/day", async (request)=>{
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        });
+
+        const { date } = getDayParams.parse(request.query);
+        const weekDay = dayjs(date).get("day");
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                cretated_at: {
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        });
+
+        return {
+            possibleHabits,
+        }
+    });
+}
